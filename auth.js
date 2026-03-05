1ef9a4a7047de438f6d82cb0351c74017b3eff00
reivieir <reinaldo.paulo@gmail.com>
2026-03-05 15:59:39 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,20 @@
+// CONFIGURAÇÕES DO SEU PROJETO SUPABASE
 const SUPABASE_URL = 'https://spcpujmqnjtalupxzqpp.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwY3B1am1xbmp0YWx1cHh6cXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MjI3ODMsImV4cCI6MjA4ODI5ODc4M30.TYVtILJNISNR3aDejRM5fsWKt9TuIHgN1dS2deVbQwQ'; // <--- COLE A CHAVE LONGA DA SUA FOTO
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwY3B1am1xbmp0YWx1cHh6cXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMDIzNjIsImV4cCI6MjA1NjY2MjM2Mn0.C-w6W307mXkG6rA369qD3y6m3R5y3m6m3R5y3m6m3R5y3m'; 
 
 const _auth = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 async function fazerLogin() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    const msg = document.getElementById('mensagem-erro');
+    
+    // Agora que o utilizador está confirmado no painel, o login passará
+    const { data, error } = await _auth.auth.signInWithPassword({ email, password });
 
-    try {
-        const { data, error } = await _auth.auth.signInWithPassword({ email, password });
-
-        if (error) {
-            msg.innerText = "Erro: " + error.message;
-        } else {
-            // Se o login der certo, ele vai para o index.html
-            window.location.href = 'index.html';
-        }
-    } catch (e) {
-        msg.innerText = "Erro de conexão.";
+    if (error) {
+        alert("Erro no Login: " + error.message);
+    } else {
+        // Sucesso! Redireciona para o painel principal
+        window.location.href = 'index.html';
     }
 }
