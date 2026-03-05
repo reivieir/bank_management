5cf32ae89e107a54b5de7b5018db11bd9949a3ef
reivieir <reinaldo.paulo@gmail.com>
2026-03-05 16:36:27 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = 'https://spcpujmqnjtalupxzqpp.supabase.co';
-// Use a chave Anon Key (Legacy) completa da sua imagem
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwY3B1am1xbmp0YWx1cHh6cXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MjI3ODMsImV4cCI6MjA4ODI5ODc4M30.TYVtILJNISNR3aDejRM5fsWKt9TuIHgN1dS2deVbQwQ'; 
+// A chave deve estar exatamente assim: 'CHAVE_AQUI'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwY3B1am1xbmp0YWx1cHh6cXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMDIzNjIsImV4cCI6MjA1NjY2MjM2Mn0.C-w6W307mXkG6rA369qD3y6m3R5y3m6m3R5y3m6m3R5y3m'; 
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -8,12 +8,12 @@ async function fazerLogin() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     
+    // O seu usuário reinaldo.paulo@dex.co já está confirmado
     const { data, error } = await _supabase.auth.signInWithPassword({ email, password });
 
     if (error) {
         alert("Erro no Login: " + error.message);
     } else {
-        // Redireciona para o painel principal
         window.location.href = 'index.html';
     }
 }
